eea9c961213bf3cd3ac5cb529ed51ef51c81e1a8
SaeMaulidin <sadammaulidin@gmail.com>
2021-03-10 11:29:21 +0700

    Initial Commit

---
+++ Sunset/Location.swift
@@ -0,0 +1,78 @@
+import Foundation
+import CoreLocation
+import MapKit
+
+
+class Location: NSObject, CLLocationManagerDelegate{
+	private var longitude: String = "Loading..."
+	private var latitude: String = "Loading..."
+	private var city: String = ""
+	private var country: String = ""
+	private let locationManager = CLLocationManager()
+	private var notificationWasSend:Bool = false
+	
+	override init() {
+		super.init()
+		
+		locationManager.delegate = self
+		locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
+		locationManager.requestWhenInUseAuthorization()
+		locationManager.requestLocation()
+	}
+	
+	func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+		DispatchQueue.main.async {
+			let userLocation:CLLocation = locations.first!
+			if !self.notificationWasSend{
+				self.notificationWasSend = true
+				self.longitude = String(format:"%f", userLocation.coordinate.longitude)
+				self.latitude = String(format:"%f", userLocation.coordinate.latitude)
+				
+				self.locationManager.stopUpdatingLocation()
+				
+				CLGeocoder().reverseGeocodeLocation(userLocation, completionHandler: {(placemarks, error)-> Void in
+					if placemarks != nil{
+						if placemarks!.count > 0{
+							let placemark = placemarks![0]
+							self.city = placemark.locality!
+							self.country = placemark.country!
+                            
+                            // VC to display new lcoation
+							NotificationCenter.default.post(name: Notification.Name("receiveLocation"), object: nil)
+                            
+                            // VC to display new sun profile 
+							NotificationCenter.default.post(name: Notification.Name("retrieveSunProfile"), object: nil)
+						}
+					}else{
+						print("Error while obtaining city")
+					}
+					if error != nil{
+						print(error!)
+					}
+				
+				})
+			}
+		}
+	}
+	
+	func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+		print("Failed to find user's location: \(error.localizedDescription)")
+	}
+	
+	
+	func getLongitude()->String{
+		return longitude
+	}
+	
+	func getLatitude()->String{
+		return latitude
+	}
+	
+	func getCity()->String{
+		return city
+	}
+	
+	func getCountry()->String{
+		return country
+	}
+}
---
+++ Sunset/Sun.swift
@@ -0,0 +1,134 @@
+import Foundation
+class Sun:NSObject{
+	
+	private var sunset: String = "Loading..."
+	private var sunrise: String = "Loading..."
+	private var dayLength: String = "Loading..."
+	
+	private var location: Location = Location()
+	
+	private var apiURL = "http://api.sunrise-sunset.org/json?lat=51.850775&lng=4.368332"
+	private var results = [String:AnyObject]()
+	
+	override init(){
+		super.init()
+		NotificationCenter.default.addObserver(self, selector: #selector(Sun.retrieveSunProfile(notification:)), name: Notification.Name("retrieveSunProfile"), object: nil)
+	}
+	
+	@objc func retrieveSunProfile(notification:Notification){
+		if Reachability.isConnectedToNetwork() == true {
+            self.apiURL = "http://api.sunrise-sunset.org/json?lat=\(location.getLatitude())&lng=\(location.getLongitude())"
+            print(self.apiURL)
+			getJSON()
+		}else{
+			print("Internet Connection not Available!")
+		}
+	}
+	
+	
+	func getSunset()->String{
+		return sunset
+	}
+	
+	func getSunrise()->String{
+		return sunrise
+	}
+	
+	func getDayLength()->String{
+		return dayLength
+	}
+	
+	func getLocation()->String{
+		return location.getCity()
+	}
+	
+	func getLongitude()->String{
+		return location.getLongitude()
+	}
+	
+	func getLatitude()->String{
+		return location.getLatitude()
+	}
+    //TODO: day length
+    func getKeys()->[String]{
+        let array = ["Sunset",
+                     "Sunrise",
+                     "Solar noon",
+                     "Civil twilight begin",
+                     "Civil twilight end",
+                     "Nautical twilight begin",
+                     "Nautical twilight end",
+                     "Astronomical twilight begin",
+                     "Astronomical twilight end"]
+        return array
+    }
+    //TODO: day length
+    //array.append(self.results["day_length"] as! String)
+    func getValues()->[Date]{
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "hh:mm:ss a"
+        
+        var array : [Date] = []
+        array.append(dateFormatter.date(from: self.results["sunset"] as! String)!)
+        array.append(dateFormatter.date(from: self.results["sunrise"] as! String)!)
+        array.append(dateFormatter.date(from: self.results["solar_noon"] as! String)!)
+        array.append(dateFormatter.date(from: self.results["civil_twilight_begin"] as! String)!)
+        array.append(dateFormatter.date(from: self.results["civil_twilight_end"] as! String)!)
+        array.append(dateFormatter.date(from: self.results["nautical_twilight_begin"] as! String)!)
+        array.append(dateFormatter.date(from: self.results["nautical_twilight_end"] as! String)!)
+        array.append(dateFormatter.date(from: self.results["astronomical_twilight_begin"] as! String)!)
+        array.append(dateFormatter.date(from: self.results["astronomical_twilight_end"] as! String)!)
+        
+        var timeZoneArray :[Date] = []
+        var secondsFromGMT: Int {return TimeZone.current.secondsFromGMT()}
+        
+        for result in array{
+            timeZoneArray.append(result.addingTimeInterval(Double(secondsFromGMT)))
+        }
+        return timeZoneArray
+    }
+	
+	func getJSON(){
+		let requestURL: NSURL = NSURL(string: apiURL)!
+		let urlRequest: NSMutableURLRequest = NSMutableURLRequest(url: requestURL as URL)
+		let session = URLSession.shared
+		let task = session.dataTask(with: urlRequest as URLRequest){
+			(data, response, error) -> Void in
+			
+			let httpResponse = response as! HTTPURLResponse
+			let statusCode = httpResponse.statusCode
+			
+			if statusCode == 200{
+				do{
+					let parsedData = try JSONSerialization.jsonObject(with: data!, options: []) as! [String:Any]
+					if let items = parsedData["results"] as? [String:AnyObject]{
+						if parsedData["status"] as? String == "OK"{
+							self.results = items
+							self.setData()
+						}
+					}else{
+						print("Error while retrieving the json")
+						let statError = parsedData["status"] as? String
+						print("API error: \(statError!)")
+					}
+				}catch{
+					print("error: \(error)")
+				}
+			}
+		}
+		task.resume()		
+	}
+	
+	func setData(){
+		self.sunset = self.results["sunset"] as! String
+		self.sunrise = self.results["sunrise"] as! String
+		self.dayLength = self.results["day_length"] as! String
+        
+		
+		NotificationCenter.default.post(name: Notification.Name("reloadData"), object: nil)
+	}
+    func getDataCount()->Int{
+        //return self.results.count
+        return 9
+    }
+}
